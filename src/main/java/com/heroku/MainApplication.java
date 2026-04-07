91381c456763649955c515538a43cf353b3cf0f7
Chris Wong <chris.wong.ksl@gmail.com>
2026-04-07 13:38:25 +0800

    Load file from current pdf folder

--- src/main/java/com/heroku/MainApplication.java
+++ src/main/java/com/heroku/MainApplication.java
@@ -1,9 +1,10 @@
 package com.heroku;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,9 @@ import java.util.Map;
 @SpringBootApplication
 @Controller
 public class MainApplication {
+    @Autowired
+    private ResourceLoader resourceLoader;
+
     @GetMapping("/")
     public String index(Map<String, Object> model) {
         model.put("message", "This is a test");
@@ -28,7 +32,7 @@ public class MainApplication {
 
     @GetMapping("/view-pdf")
     public ResponseEntity<Resource> viewPdf() {
-        Resource pdf = new ClassPathResource("static/sample.pdf");
+        Resource pdf = resourceLoader.getResource("file:./pdf/sample.pdf");
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_PDF)
                 .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"sample.pdf\"")
