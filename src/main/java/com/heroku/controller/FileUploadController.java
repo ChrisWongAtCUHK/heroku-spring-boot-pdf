292f5e1e212441449a31dfa2d28d75c0318f21b3
Chris Wong <chris.wong.ksl@gmail.com>
2026-04-07 13:05:09 +0800

    Save file to main/resources/static

--- src/main/java/com/heroku/controller/FileUploadController.java
+++ src/main/java/com/heroku/controller/FileUploadController.java
@@ -1,5 +1,6 @@
 package com.heroku.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +9,18 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class FileUploadController {
 
-    @PostMapping("/file-upload")
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public String handleFileUpload(@RequestParam("file") MultipartFile file) {
         if (file.isEmpty()) {
             return "File is empty";
         }
-        // Access file info: file.getOriginalFilename(), file.getContentType(), file.getSize()
-        return "Successfully uploaded: " + file.getOriginalFilename();
+        // Access file properties
+        String fileName = file.getOriginalFilename();
+        long size = file.getSize();
+
+        // Save the file or process bytes
+        // file.transferTo(new File("/path/to/destination/" + fileName))
+
+        return "Successfully uploaded: " + fileName + "(" + size +")";
     }
 }
